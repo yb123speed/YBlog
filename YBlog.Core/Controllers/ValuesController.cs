a4c06eabd3eed820ef4b3fc605f0a929fc4d5839
yb123speed <904896209@qq.com>
2018-10-13 17:08:00 +0800

    Add ValuesContronller Comment And Change csproj File to hide CS1591 Warnning.
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/Controllers/ValuesController.cs
+++ YBlog.Core/Controllers/ValuesController.cs
@@ -6,11 +6,18 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace YBlog.Core.Controllers
 {
+    /// <summary>
+    /// Values控制器
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
@@ -18,6 +25,11 @@ namespace YBlog.Core.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// 根据Id获取值
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -25,18 +37,33 @@ namespace YBlog.Core.Controllers
         }
 
         // POST api/values
+        /// <summary>
+        /// 创建对象
+        /// </summary>
+        /// <param name="value"></param>
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
         // PUT api/values/5
+
+        /// <summary>
+        /// 更新对象
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="value"></param>
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/values/5
+
+        /// <summary>
+        /// 删除对象
+        /// </summary>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
