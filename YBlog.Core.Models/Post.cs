96baebf76e351ea9eb3c2ec853d066a73f701064
yb123speed <904896209@qq.com>
2018-10-14 23:38:23 +0800

    add Post Class

---
+++ YBlog.Core.Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YBlog.Core.Models
+{
+    /// <summary>
+    /// Post
+    /// </summary>
+    public class Post
+    {
+
+    }
+}
