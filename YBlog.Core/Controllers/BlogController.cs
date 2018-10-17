1dd955a9fb7cb44937f2326d69f43930ae2df067
yb123speed <904896209@qq.com>
2018-10-17 16:20:59 +0800

    添加BlogController以及示例代码
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core/Controllers/BlogController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YBlog.Core.IServices;
+using YBlog.Core.Services;
+
+namespace YBlog.Core.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Policy = "Admin")]
+    public class BlogController : ControllerBase
+    {
+        // GET: api/Blog
+        [HttpGet]
+        public int Get(int i, int j)
+        {
+            IAdvertisementServices advertisementServices = new AdvertisementServices();
+            return advertisementServices.Sum(i,j);
+        }
+
+        // GET: api/Blog/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Blog
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Blog/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
