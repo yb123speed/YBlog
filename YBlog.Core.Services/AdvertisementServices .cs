dfcf48aa7dd3755ea79eb600a398315287e6be7d
yb123speed <904896209@qq.com>
2018-10-17 15:52:02 +0800

    添加示例代码
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core.Services/AdvertisementServices .cs
+++ YBlog.Core.Services/AdvertisementServices .cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using YBlog.Core.IRepository;
+using YBlog.Core.IServices;
+using YBlog.Core.Repository;
 
 namespace YBlog.Core.Services
 {
-    public class AdvertisementServices
+    public class AdvertisementServices: IAdvertisementServices
     {
+        IAdvertisementRepository dal = new AdvertisementRepository();
+
+        public int Sum(int i, int j)
+        {
+            return dal.Sum(i, j);
+        }
     }
 }
