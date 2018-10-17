b59f80b78dcf346dd95d07b7c8139cca935b8418
yb123speed <904896209@qq.com>
2018-10-17 14:36:10 +0800

    添加Repository和IRepository类库
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core.IRepository/IAdvertisementRepository.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace YBlog.Core.IRepository
+{
+    public interface IAdvertisementRepository
+    {
+        int Sum(int i, int j);
+    }
+}
---
+++ YBlog.Core.Repository/AdvertisementRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YBlog.Core.IRepository;
+
+namespace YBlog.Core.Repository
+{
+    public class AdvertisementRepository : IAdvertisementRepository
+    {
+        public int Sum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
