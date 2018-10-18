f409c2b0bd4929861653636a24108c36b82d71d4
yb123speed <904896209@qq.com>
2018-10-18 09:47:53 +0800

    Models新增Advertisement 类    IRepository新增CURD声明

--- YBlog.Core.IRepository/IAdvertisementRepository.cs
+++ YBlog.Core.IRepository/IAdvertisementRepository.cs
@@ -1,9 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using YBlog.Core.Models.Models;
 
 namespace YBlog.Core.IRepository
 {
     public interface IAdvertisementRepository
     {
         int Sum(int i, int j);
+
+
+        int Add(Advertisement model);
+        bool Delete(Advertisement model);
+        bool Update(Advertisement model);
+        List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression);
     }
 }
