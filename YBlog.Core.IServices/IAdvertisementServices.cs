8dec4977e654223c8c34efbaa39195dc5b5c4e7f
yb123speed <904896209@qq.com>
2018-10-18 17:05:09 +0800

    IAdvertisementServices新增方法声明并在AdvertisementServices里实现
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core.IServices/IAdvertisementServices.cs
+++ YBlog.Core.IServices/IAdvertisementServices.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using YBlog.Core.Models.Models;
 
 namespace YBlog.Core.IServices
 {
     public interface IAdvertisementServices
     {
         int Sum(int i, int j);
+        int Add(Advertisement model);
+        bool Delete(Advertisement model);
+        bool Update(Advertisement model);
+        List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression);
     }
 }
--- YBlog.Core.Services/AdvertisementServices .cs
+++ YBlog.Core.Services/AdvertisementServices .cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using YBlog.Core.IRepository;
 using YBlog.Core.IServices;
+using YBlog.Core.Models.Models;
 using YBlog.Core.Repository;
 
 namespace YBlog.Core.Services
@@ -9,9 +12,29 @@ namespace YBlog.Core.Services
     {
         IAdvertisementRepository dal = new AdvertisementRepository();
 
+        public int Add(Advertisement model)
+        {
+            return dal.Add(model);
+        }
+
+        public bool Delete(Advertisement model)
+        {
+            return dal.Delete(model);
+        }
+
+        public List<Advertisement> Query(Expression<Func<Advertisement, bool>> whereExpression)
+        {
+            return dal.Query(whereExpression);
+        }
+
         public int Sum(int i, int j)
         {
             return dal.Sum(i, j);
+        }
+
+        public bool Update(Advertisement model)
+        {
+            return dal.Update(model);
         }
     }
 }
