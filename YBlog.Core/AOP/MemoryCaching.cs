42c8e5b8ee61c76ff72cc48610aa4170bac41ac6
yb123speed <904896209@qq.com>
2018-10-22 15:36:11 +0800

    添加Cache相关代码
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core/AOP/MemoryCaching.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YBlog.Core.AOP
+{
+    /// <summary>
+    /// 实例化缓存接口ICaching
+    /// </summary>
+    public class MemoryCaching : ICaching
+    {
+        #region private fileds and constructor
+
+        //引用Microsoft.Extensions.Caching.Memory;这个和.net 还是不一样，没有了Httpruntime了
+        private readonly IMemoryCache _memoryCache;
+
+        public MemoryCaching(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        #endregion
+        public object Get(string cacheKey)
+        {
+            return _memoryCache.Get(cacheKey);
+        }
+
+        public void Set(string cacheKey, object cacheValue)
+        {
+            _memoryCache.Set(cacheKey, cacheValue, TimeSpan.FromHours(2));
+        }
+    }
+}
