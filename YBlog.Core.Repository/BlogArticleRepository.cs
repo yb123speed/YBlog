08a1214c2501fe66044d5ee4b24d1b2aac4013ec
yb123speed <904896209@qq.com>
2018-10-22 10:05:18 +0800

    添加BlogArticle模型类并实现对应的IBlogArticleRepository和BlogArticleRepository
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core.Repository/BlogArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using YBlog.Core.IRepository;
+using YBlog.Core.Models.Models;
+using YBlog.Core.Repository.Base;
+
+namespace YBlog.Core.Repository
+{
+    public class BlogArticleRepository : BaseRepository<BlogArticle>, IBlogArticleRepository
+    {
+        
+    }
+}
