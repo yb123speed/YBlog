9941701aeadfabb8e2526405b4b8a8bf82f3fa02
yb123speed <904896209@qq.com>
2018-10-22 10:31:33 +0800

    添加BlogArticle对应IServices和Services的实现
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core.IServices/IBlogArticleServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YBlog.Core.IServices.Base;
+using YBlog.Core.Models.Models;
+
+namespace YBlog.Core.IServices
+{
+    public interface IBlogArticleServices:IBaseServices<BlogArticle>
+    {
+        Task<List<BlogArticle>> GetBlogs();
+    }
+}
---
+++ YBlog.Core.Services/BlogArticleServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using YBlog.Core.IRepository;
+using YBlog.Core.IServices;
+using YBlog.Core.Models.Models;
+using YBlog.Core.Services.Base;
+
+namespace YBlog.Core.Services
+{
+    public class BlogArticleServices : BaseServices<BlogArticle>, IBlogArticleServices
+    {
+        #region private fields and constructor
+
+        IBlogArticleRepository _blogArticleRepository;
+
+        public BlogArticleServices(IBlogArticleRepository blogArticleRepository)
+        {
+            _blogArticleRepository = blogArticleRepository;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 获取博客列表
+        /// </summary>
+        /// <returns></returns>
+        public async Task<List<BlogArticle>> GetBlogs()
+        {
+            var blogList = await _blogArticleRepository.Query(a => a.BId > 0, a => a.BId);
+            return blogList;
+        }
+    }
+}
