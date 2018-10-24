142da95c57c69e777a2bd55790698cb19375f0f8
yb123speed <904896209@qq.com>
2018-10-24 09:55:05 +0800

    补充CORS策略名
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/Startup.cs
+++ YBlog.Core/Startup.cs
@@ -66,7 +66,7 @@ namespace YBlog.Core
                 //↑↑↑↑↑↑↑注意正式环境不要使用这种全开放的处理↑↑↑↑↑↑↑↑↑↑
 
                 //一般采用这种方法
-                o.AddPolicy("", policy=> {
+                o.AddPolicy("LimitRequests", policy=> {
                     policy
                     .WithOrigins("http://127.0.0.1:1818", "http://localhost:8080", "http://localhost:8021", "http://localhost:8081", "http://localhost:1818", "http://blog.core.xxx.com", "")//支持多个域名端口
                     .WithMethods("GET", "POST", "PUT", "DELETE")//请求方法添加到策略
