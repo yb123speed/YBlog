efaaa19c84baca033fe783e11b3fbaa6a7c03fa3
yb123speed <904896209@qq.com>
2018-10-25 13:35:02 +0800

    添加BlogViewModel并增加获取文章详细的Services实现
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core.Models/ViewModels/BlogViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YBlog.Core.Models.ViewModels
+{
+    /// <summary>
+    /// 博客信息展示类
+    /// </summary>
+    public class BlogViewModel
+    {
+        /// <summary>
+        /// Blog Id
+        /// </summary>
+        public int BId { get; set; }
+        /// <summary>/// 创建人
+        /// </summary>
+        public string BSubmitter { get; set; }
+
+        /// <summary>/// 博客标题
+        /// </summary>
+        public string BTitle { get; set; }
+
+        /// <summary>/// 摘要
+        /// </summary>
+        public string Digest { get; set; }
+
+        /// <summary>
+        /// 上一篇
+        /// </summary>
+        public string Previous { get; set; }
+
+        /// <summary>
+        /// 上一篇id
+        /// </summary>
+        public int PreviousId { get; set; }
+
+        /// <summary>
+        /// 下一篇
+        /// </summary>
+        public string Next { get; set; }
+
+        /// <summary>
+        /// 下一篇id
+        /// </summary>
+        public int NextId { get; set; }
+
+        /// <summary>/// 类别
+        /// </summary>
+        public string BCategory { get; set; }
+
+        /// <summary>/// 内容
+        /// </summary>
+        public string BContent { get; set; }
+
+        /// <summary>
+        /// 访问量
+        /// </summary>
+        public int BTraffic { get; set; }
+
+        /// <summary>
+        /// 评论数量
+        /// </summary>
+        public int BCommentNum { get; set; }
+
+        /// <summary>/// 修改时间
+        /// </summary>
+        public DateTime BUpdateTime { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public System.DateTime BCreateTime { get; set; }
+        /// <summary>/// 备注
+        /// </summary>
+        public string BRemark { get; set; }
+    }
+}
--- YBlog.Core.Services/BlogArticleServices.cs
+++ YBlog.Core.Services/BlogArticleServices.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YBlog.Core.IRepository;
 using YBlog.Core.IServices;
 using YBlog.Core.Models.Models;
+using YBlog.Core.Models.ViewModels;
 using YBlog.Core.Services.Base;
 
 namespace YBlog.Core.Services
@@ -31,5 +33,73 @@ namespace YBlog.Core.Services
             var blogList = await _blogArticleRepository.Query(a => a.BId > 0, a => a.BId);
             return blogList;
         }
+
+        public async Task<BlogViewModel> GetBlogDetails(int id)
+        {
+            var bloglist = await _blogArticleRepository.Query(a => a.BId > 0, a => a.BId);
+            var idmin = bloglist.FirstOrDefault() != null ? bloglist.FirstOrDefault().BId : 0;
+            var idmax = bloglist.LastOrDefault() != null ? bloglist.LastOrDefault().BId : 1;
+            var idminshow = id;
+            var idmaxshow = id;
+
+            BlogArticle blogArticle = new BlogArticle();
+
+            blogArticle = (await _blogArticleRepository.Query(a => a.BId == idminshow)).FirstOrDefault();
+
+            BlogArticle prevblog = new BlogArticle();
+
+
+            while (idminshow > idmin)
+            {
+                idminshow--;
+                prevblog = (await _blogArticleRepository.Query(a => a.BId == idminshow)).FirstOrDefault();
+                if (prevblog != null)
+                {
+                    break;
+                }
+            }
+
+            BlogArticle nextblog = new BlogArticle();
+            while (idmaxshow < idmax)
+            {
+                idmaxshow++;
+                nextblog = (await _blogArticleRepository.Query(a => a.BId == idmaxshow)).FirstOrDefault();
+                if (nextblog != null)
+                {
+                    break;
+                }
+            }
+
+
+            blogArticle.BTraffic += 1;
+            await _blogArticleRepository.Update(blogArticle, new List<string> { "BTraffic" });
+
+            BlogViewModel models = new BlogViewModel()
+            {
+                BSubmitter = blogArticle.BSubmitter,
+                BTitle = blogArticle.BTitle,
+                BCategory = blogArticle.BCategory,
+                BContent = blogArticle.BContent,
+                BTraffic = blogArticle.BTraffic,
+                BCommentNum = blogArticle.BCommentNum,
+                BUpdateTime = blogArticle.BUpdateTime,
+                BCreateTime = blogArticle.BCreateTime,
+                BRemark = blogArticle.BRemark,
+            };
+
+            if (nextblog != null)
+            {
+                models.Next = nextblog.BTitle;
+                models.NextId = nextblog.BId;
+            }
+
+            if (prevblog != null)
+            {
+                models.Previous = prevblog.BTitle;
+                models.PreviousId = prevblog.BId;
+            }
+            return models;
+        }
+
     }
 }
