4b086d992c4cae5b6db303dcbbed03a98cb3816d
yb123speed <904896209@qq.com>
2018-10-25 11:24:29 +0800

    LoginController使用CORS策略
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/Controllers/LoginController.cs
+++ YBlog.Core/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using YBlog.Core.AuthHelper;
@@ -11,6 +12,7 @@ namespace YBlog.Core.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [EnableCors("LimitRequests")]
     public class LoginController : ControllerBase
     {
         /// <summary>
