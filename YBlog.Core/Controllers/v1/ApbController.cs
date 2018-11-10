61d051f511e59b7a3196989f016e3df4411ca9eb
yb123speed <904896209@qq.com>
2018-11-10 17:13:41 +0800

    同名API处理
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core/Controllers/v1/ApbController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YBlog.Core.SwaggerHelper;
+using static YBlog.Core.SwaggerHelper.CustomApiVersion;
+
+namespace YBlog.Core.Controllers.v1
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApbController : ControllerBase
+    {
+        [HttpGet]
+        [CustomRoute(ApiVersions.v1, "apbs")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "第一版的 apbs" };
+        }
+    }
+}
---
+++ YBlog.Core/Controllers/v2/ApbController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using YBlog.Core.SwaggerHelper;
+using static YBlog.Core.SwaggerHelper.CustomApiVersion;
+
+namespace YBlog.Core.Controllers.v2
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApbController : ControllerBase
+    {
+        [HttpGet]
+        [CustomRoute(ApiVersions.v2, "apbs")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "第二版的 apbs" };
+        }
+    }
+}
