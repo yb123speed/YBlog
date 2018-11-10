c62990b5fba09e0a0f802d11e34df43471360a3f
yb123speed <904896209@qq.com>
2018-11-10 15:59:26 +0800

    对接口进行版本配置
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/Controllers/BlogController.cs
+++ YBlog.Core/Controllers/BlogController.cs
@@ -109,5 +109,14 @@ namespace YBlog.Core.Controllers
             }
             return blogs;
         }
+
+        [HttpGet]
+        [ApiExplorerSettings(GroupName = "v2")]
+        //路径 如果以 / 开头，表示绝对路径，反之相对 controller 的相对路径
+        [Route("/api/v2/blog/Blogtest")]
+        public object V2_Blogtest()
+        {
+            return Ok(new { status = 220, data = "我是第二版的博客信息" });
+        }
     }
 }
