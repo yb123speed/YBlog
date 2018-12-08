3925a584f595e2b10528c547e89b001de39c2969
yb123speed <904896209@qq.com>
2018-12-08 12:11:27 +0800

    定义了过期时间和Role
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/AuthHelper/OverWrite/JwtHelper.cs
+++ YBlog.Core/AuthHelper/OverWrite/JwtHelper.cs
@@ -21,12 +21,26 @@ namespace YBlog.Core.AuthHelper
         public static string IssueJWT(TokenModelJWT tokenModel)
         {
             var dateTime = DateTime.UtcNow;
+            //var claims = new Claim[]
+            //{
+            //    new Claim(JwtRegisteredClaimNames.Jti,tokenModel.Uid.ToString()),//Id
+            //    new Claim("Role", tokenModel.Role),//角色
+            //    new Claim(JwtRegisteredClaimNames.Iat,dateTime.ToString(),ClaimValueTypes.Integer64)
+            //};
+
             var claims = new Claim[]
             {
-                new Claim(JwtRegisteredClaimNames.Jti,tokenModel.Uid.ToString()),//Id
-                new Claim("Role", tokenModel.Role),//角色
-                new Claim(JwtRegisteredClaimNames.Iat,dateTime.ToString(),ClaimValueTypes.Integer64)
+                new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
+                new Claim(JwtRegisteredClaimNames.Nbf,$"{new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds()}"),
+                //过期时间，目前是过期100秒，可以自定义，注意Jwt有自己的缓冲过期时间
+                new Claim(JwtRegisteredClaimNames.Exp,$"{new DateTimeOffset(DateTime.Now.AddSeconds(100)).ToUnixTimeSeconds()}"),
+                new Claim(JwtRegisteredClaimNames.Iss,"YBlog.Core"),
+                new Claim(JwtRegisteredClaimNames.Aud,"wr"),
+                //这个Role是官方UseAuthentication要验证的Role,我们不用手动设置这个Role的属性了
+                new Claim(ClaimTypes.Role,tokenModel.Role),
             };
+
             //秘钥
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtHelper.secretKey));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
