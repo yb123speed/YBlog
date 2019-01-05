f2b306a7ffdd10096df1f1518d1a2bef54dea0c2
yb123speed <904896209@qq.com>
2019-01-05 14:16:56 +0800

    JwtToken生成类
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core/AuthHelper/Policys/JwtToken.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace YBlog.Core.AuthHelper
+{
+    //这个很简单，就是之前的 Token 字符串生成类，只是要注意一下下边红色的参数 PermissionRequirement ，数据是从Startup.cs 中注入的
+
+    /// <summary>
+    /// JwtToken生成类
+    /// </summary>
+    public class JwtToken
+    {
+        public static dynamic BuildJwtToken(Claim[] claims, PermissionRequirement permissionRequirement)
+        {
+            var now = DateTime.Now;
+            //实例化JwtSecurityToken
+            var jwt = new JwtSecurityToken(
+                issuer: permissionRequirement.Issuer,
+                audience: permissionRequirement.Audience,
+                claims: claims,
+                notBefore: now,
+                expires: now.Add(permissionRequirement.Expiration),
+                signingCredentials: permissionRequirement.SigningCredentials
+            );
+
+            //生成  Token
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            //打包返回前台
+            var responseJson = new
+            {
+                success = true,
+                token = encodedJwt,
+                expires_in = permissionRequirement.Expiration.TotalSeconds,
+                token_type = "Bearer"
+            };
+            return responseJson;
+        }
+    }
+}
