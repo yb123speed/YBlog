e69320550e5b585f04b011f853f00289850fc674
yb123speed <904896209@qq.com>
2019-01-05 14:18:30 +0800

    PermissionHandler的必要参数类PermissionRequirement
    Signed-off-by: yb123speed <904896209@qq.com>

---
+++ YBlog.Core/AuthHelper/Policys/PermissionRequirement.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace YBlog.Core.AuthHelper
+{
+    //这里边存放的都是 Jwt Token 的全部信息，注意它继承了 IAuthorizationRequirement，因为我们要设计自定义授权验证处理器，所以必须继承验证要求接口，才能设计我们自己的参数：
+
+    /// <summary>
+    /// 必要参数类，
+    /// 继承 IAuthorizationRequirement，用于设计自定义权限处理器PermissionHandler
+    /// 因为AuthorizationHandler 中的泛型参数 TRequirement 必须继承 IAuthorizationRequirement
+    /// </summary>
+    public class PermissionRequirement:IAuthorizationRequirement
+    {
+        /// <summary>
+        /// 构造
+        /// </summary>
+        /// <param name="deniedAction">拒约请求的url</param>
+        /// <param name="permissions">权限集合</param>
+        /// <param name="claimType">声明类型</param>
+        /// <param name="issuer">发行人</param>
+        /// <param name="audience">订阅人</param>
+        /// <param name="signingCredentials">签名验证实体</param>
+        /// <param name="expiration">过期时间</param>
+        public PermissionRequirement(string deniedAction, List<Permission> permissions, string claimType, string issuer, string audience, SigningCredentials signingCredentials, TimeSpan expiration)
+        {
+            Permissions = permissions;
+            DeniedAction = deniedAction;
+            ClaimType = claimType;
+            Issuer = issuer;
+            Audience = audience;
+            Expiration = expiration;
+            SigningCredentials = signingCredentials;
+        }
+
+        /// <summary>
+        /// 用户权限集合
+        /// </summary>
+        public List<Permission> Permissions { get; set; }
+
+        /// <summary>
+        /// 无权限action
+        /// </summary>
+        public string DeniedAction { get; set; }
+
+        /// <summary>
+        /// 认证授权类型
+        /// </summary>
+        public string ClaimType { internal get; set; }
+
+        /// <summary>
+        /// 请求路径
+        /// </summary>
+        public string LoginPath { get; set; } = "/Api/Login";
+
+        /// <summary>
+        /// 发行人
+        /// </summary>
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// 订阅人
+        /// </summary>
+        public string Audience { get; set; }
+
+        /// <summary>
+        /// 过期时间
+        /// </summary>
+        public TimeSpan Expiration { get; set; }
+
+        /// <summary>
+        /// 签名验证
+        /// </summary>
+        public SigningCredentials SigningCredentials { get; set; }
+    }
+}
