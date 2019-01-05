b8831540b6de46b72422bf91c67b5aa9945785a5
yb123speed <904896209@qq.com>
2019-01-05 17:21:44 +0800

    修改JWT服务注册方法
    Signed-off-by: yb123speed <904896209@qq.com>

--- YBlog.Core/Startup.cs
+++ YBlog.Core/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using Autofac;
@@ -13,6 +14,7 @@ using log4net;
 using log4net.Config;
 using log4net.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -71,6 +73,14 @@ namespace YBlog.Core
             services.AddScoped<ICaching, MemoryCaching>();
             services.AddScoped<IRedisCacheManager, RedisCacheManager>();
             services.AddSingleton<ILoggerHelper, LogHelper>();
+            services.AddSingleton<IMemoryCache>(factory =>
+            {
+                var cache = new MemoryCache(new MemoryCacheOptions());
+                return cache;
+            });
+
+            // 依赖注入，将自定义的授权处理器 匹配给官方授权处理器接口，这样当系统处理授权的时候，就会直接访问我们自定义的授权处理器了。
+            services.AddSingleton<IAuthorizationHandler, PermissionHandler>();
 
             #endregion
 
@@ -168,48 +178,87 @@ namespace YBlog.Core
             });
             #endregion
 
-            #region JwtBearer认证
-            services.AddAuthentication(o=> 
-            {
-                o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(o => 
-            {
-                o.TokenValidationParameters = new TokenValidationParameters
-                {
-                    //ValidIssuer = "YBlog.Core",
-                    //ValidAudience = "wr",
-                    //IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtHelper.secretKey)),
-                    //RequireSignedTokens = true,
-                    //// 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
-                    //ValidateAudience = false,
-                    //ValidateIssuer = true,
-                    //ValidateIssuerSigningKey = true,
-                    //// 是否要求Token的Claims中必须包含 Expires
-                    //RequireExpirationTime = true,
-                    //// 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
-                    //ValidateLifetime = true
+            //#region JwtBearer认证
+            //services.AddAuthentication(o=> 
+            //{
+            //    o.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            //    o.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            //})
+            //.AddJwtBearer(o => 
+            //{
+            //    o.TokenValidationParameters = new TokenValidationParameters
+            //    {
+            //        //ValidIssuer = "YBlog.Core",
+            //        //ValidAudience = "wr",
+            //        //IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtHelper.secretKey)),
+            //        //RequireSignedTokens = true,
+            //        //// 将下面两个参数设置为false，可以不验证Issuer和Audience，但是不建议这样做。
+            //        //ValidateAudience = false,
+            //        //ValidateIssuer = true,
+            //        //ValidateIssuerSigningKey = true,
+            //        //// 是否要求Token的Claims中必须包含 Expires
+            //        //RequireExpirationTime = true,
+            //        //// 是否验证Token有效期，使用当前时间与Token的Claims中的NotBefore和Expires对比
+            //        //ValidateLifetime = true
 
-                    ValidateIssuer = true,//是否验证Issuer
-                    ValidateAudience = true,//是否验证Audience 
-                    ValidateIssuerSigningKey = true,//是否验证IssuerSigningKey 
-                    ValidIssuer = "YBlog.Core",
-                    ValidAudience = "wr",
-                    ValidateLifetime = true,//是否验证超时  当设置exp和nbf时有效 同时启用ClockSkew 
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtHelper.secretKey)),
-                    //注意这是缓冲过期时间，总的有效时间等于这个时间加上jwt的过期时间，如果不配置，默认是5分钟
-                    ClockSkew = TimeSpan.FromSeconds(30)
-                };
-            });
-            #endregion
+            //        ValidateIssuer = true,//是否验证Issuer
+            //        ValidateAudience = true,//是否验证Audience 
+            //        ValidateIssuerSigningKey = true,//是否验证IssuerSigningKey 
+            //        ValidIssuer = "YBlog.Core",
+            //        ValidAudience = "wr",
+            //        ValidateLifetime = true,//是否验证超时  当设置exp和nbf时有效 同时启用ClockSkew 
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(JwtHelper.secretKey)),
+            //        //注意这是缓冲过期时间，总的有效时间等于这个时间加上jwt的过期时间，如果不配置，默认是5分钟
+            //        ClockSkew = TimeSpan.FromSeconds(30)
+            //    };
+            //});
+            //#endregion
 
             #region Token服务注册
-            services.AddSingleton<IMemoryCache>(factory =>
+
+            //读取配置文件
+            var audienceConf = Configuration.GetSection("Audience");
+            var symmetricKeyAsBase64 = audienceConf["Secret"];
+            var keyByteArray = Encoding.ASCII.GetBytes(symmetricKeyAsBase64);
+            var signingKey = new SymmetricSecurityKey(keyByteArray);
+
+            // 令牌验证参数，之前我们都是写在AddJwtBearer里的，这里提出来了
+            var tokenValidationParameters = new TokenValidationParameters
             {
-                var cache = new MemoryCache(new MemoryCacheOptions());
-                return cache;
-            });
+                ValidateIssuerSigningKey = true,//验证发行人的签名密钥
+                IssuerSigningKey = signingKey,
+                ValidateIssuer = true,//验证发行人
+                ValidIssuer = audienceConf["Issuer"],//发行人
+                ValidateAudience = true,//验证订阅人
+                ValidAudience = audienceConf["Audience"],//订阅人
+                ValidateLifetime = true,//验证生命周期
+                ClockSkew = TimeSpan.Zero,//这个是定义的过期的缓存时间
+                RequireExpirationTime = true,//是否要求过期
+
+            };
+            var signingCredentials = new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
+
+            // 注意使用RESTful风格的接口会更好，因为只需要写一个Url即可，比如：/api/values 代表了Get Post Put Delete等多个。
+            // 如果想写死，可以直接在这里写。
+            //var permission = new List<Permission> {
+            //                  new Permission {  Url="/api/values", Role="Admin"},
+            //                  new Permission {  Url="/api/values", Role="System"},
+            //                  new Permission {  Url="/api/claims", Role="Admin"},
+            //              };
+
+            // 如果要数据库动态绑定，这里先留个空，后边处理器里动态赋值
+            var permission = new List<Permission>();
+
+            // 角色与接口的权限要求参数
+            var permissionRequirement = new PermissionRequirement(
+                "/api/denied",// 拒绝授权的跳转地址（目前无用）
+                permission,//这里还记得么，就是我们上边说到的角色地址信息凭据实体类 Permission
+                ClaimTypes.Role,//基于角色的授权
+                audienceConf["Issuer"],//发行人
+                audienceConf["Audience"],//订阅人
+                signingCredentials,//签名凭据
+                expiration: TimeSpan.FromSeconds(60 * 2)//接口的过期时间，注意这里没有了缓冲时间，你也可以自定义，在上边的TokenValidationParameters的 ClockSkew
+                );
 
             services.AddAuthorization(o =>
             {
@@ -219,7 +268,25 @@ namespace YBlog.Core
                 //options.AddPolicy("AdminOrClient", policy => policy.RequireRole("Admin,Client").Build());
                 //这个才是或的关系
                 o.AddPolicy("AdminOrClient", policy => policy.RequireRole("Admin","Client"));
+
+                // 自定义基于策略的授权权限
+                o.AddPolicy("Permission",
+                         policy => policy.Requirements.Add(permissionRequirement));
+            })
+
+            .AddAuthentication(x =>
+             {
+                 x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                 x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+             })
+
+            .AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = tokenValidationParameters;
             });
+
+            // 将授权必要类注入生命周期内
+            services.AddSingleton(permissionRequirement);
             #endregion
 
             #region AutoFac
